0c646c2e84cc001bdadf1576df13a46a203c67b5
Heba <hebamagde@gmail.com>
2024-09-04 01:47:52 +0300

    Task 6 and 7: Basic auth / Basic - Base64 part

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Basic authentication module
+"""
+import binascii
+from api.v1.auth.auth import Auth
+from base64 import b64decode
+from models.user import User
+from typing import Optional, Tuple, TypeVar
+
+
+class BasicAuth(Auth):
+    """ Basic authentication class
+    """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ Retrieves authentication parameters form authorization header
+        """
+        if not authorization_header or not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
