40b9fa2421df5ea9bfbb3e3796cd23bfc9f9dfae
Heba <hebamagde@gmail.com>
2024-09-05 22:02:31 +0300

    Task 1: Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Module of session authentication
+"""
+from api.v1.auth.auth import Auth
+from models.user import User
+from typing import Dict, TypeVar
+from uuid import uuid4
+from os import getenv
+
+
+
+class SessionAuth(Auth):
+    """ validate if everything inherits correctly without any overloading
+    and the “switch” by using environment variables
+    """
