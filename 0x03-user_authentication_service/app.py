9997be8a1242e6134a644f8946d5858a633dc8ee
Heba <hebamagde@gmail.com>
2024-09-12 19:16:09 +0300

    Task 15: User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -59,5 +59,17 @@ def logout():
     return redirect(url_for("home"))
 
 
+@app.route("/profile", methods=["GET"])
+def profile():
+    """Profile endpoint
+        Return: User profile JSON represented
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
