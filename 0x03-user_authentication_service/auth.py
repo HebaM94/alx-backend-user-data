bacfd1d24e668dc7853abd703584ec97e16649fa
Heba <hebamagde@gmail.com>
2024-09-12 09:12:58 +0300

    Task 5: Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -12,3 +12,27 @@ def _hash_password(password: str) -> str:
     """Hash password
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user
+            Args:
+                - email: user's email
+                - password: user's password
+            Return: User instance created
+        """
+        db = self._db
+        try:
+            user = db.find_user_by(email=email)
+        except NoResultFound:
+            user = db.add_user(email, _hash_password(password))
+            return user
+        else:
+            raise ValueError(f"User {email} already exists")
